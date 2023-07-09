b0b08613294db704e2580958da0c695efa1f2965
Veeranjaney <139045371+Veeranjaney@users.noreply.github.com>
2023-07-09 15:24:52 +0530

    Update first.java
    i added comment to the code section

--- first.java
+++ first.java
@@ -1,5 +1,7 @@
 class HallowWorld{
     public static void main(String[] args){
       System.out.println("this is first program");
+
+        //this is my first program in this coding journey
     }  
   }
