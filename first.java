e90302140d30516e2d7ce0cfd9b333b8990b9e6b
Veeranjaney <139045371+Veeranjaney@users.noreply.github.com>
2023-07-09 15:40:15 +0530

    Update first.java
    Added one constructor to the class

--- first.java
+++ first.java
@@ -1,6 +1,10 @@
 class HallowWorld{
+    HalloWorld(){
+        System.out.println("this is the constructor of the the class");
+    }
     public static void main(String[] args){
       System.out.println("this is first program");
+        HallowWorld w=new HallowWorld();
 
         //this is my first program in this coding journey
     }  
