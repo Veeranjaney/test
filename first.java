02350d9638c82d2f8c4e5313040ab0d8407fb073
Veeranjaney <139045371+Veeranjaney@users.noreply.github.com>
2023-07-09 16:18:15 +0530

    Update first.java
    new version where both the method and constructure are bought in single program

--- first.java
+++ first.java
@@ -1,13 +1,20 @@
 class HallowWorld{
-
+    HallowWorld(){
+    System.out.println("this is from the probability 3");
     }
+
+static void meth1(){
+
+            System.out.println("we created one method in the classs ");
+    }
+ }
     public static void main(String[] args){
       System.out.println("this is first program");
         HallowWorld w=new HallowWorld();
-
         meth1();
+        
         //we have created the static methed, so we can call that without object creation.
-
+        //this is new version where both method and constructors are crated.
         //this is my first program in this coding journey
     }  
   }
